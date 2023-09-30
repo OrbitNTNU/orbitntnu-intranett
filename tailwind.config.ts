c95df0acc423af5c635d5e23bd4663b1dae40ed1
Hanne <hannmhaa@stud.ntnu.no>
2023-09-30 10:29:51 +0200

    Add: Custom colors to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#171026',
+        secondaryColorOne: '#586F7C',
+        secondaryColorTwo: '#271F38',
+        accentColorOne: '#B8DBD9',
+        accentColorTwo: '#EAB308',
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
