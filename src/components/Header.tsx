54f6df71f0d365df3b8b90b9760f23b81e3f6fbb
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-02 19:58:17 +0200

    feat: add burger menu bg color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
 
   return (
     <header className="outline-dotted outline-red-500 flex w-screen h-10 justify-between items-center">
-      <div><img className=" w-[25vw] h-auto relative p-2" src={logoWhite.src} alt="Website logo" /></div>
-      <div><input className="bg-transparent" type="text" placeholder="Search..."/></div>
+      <div className="flex justify-start"><img className=" h-[6vh] h-auto relative" src={logoWhite.src} alt="Website logo" /></div>
+      <div><input className="bg-transparent w-[300px] relative right-18" type="text" placeholder="Search..."/></div>
       {<button style={{outline: "1px solid black"}} onClick={() => setShow(!show)}>Hamburger</button>}
       {show && 
-        <div className="absolute top-0 right-0 m-12 flex flex-col">
+        <div className="absolute top-0 right-0 m-12 flex flex-col bg-accentColorTwo ">
         <p>Hallo</p>
         <p>Hallo2</p>
         </div>
