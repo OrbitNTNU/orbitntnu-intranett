3e288ac18db233b125d0fd53a22b13a8ce27b3cb
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-02 19:28:33 +0200

    feat: add logo to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
+import logoWhite from "../../public/images/orbitwhite.png"; // Import the image file
+
 
 const Header = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <header className="outline-dotted outline-red-500 flex w-screen justify-between">
-      <div><img src="" alt="" />Bilde går her</div>
-      <div><input type="text" placeholder="Search"/></div>
+    <header className="outline-dotted outline-red-500 flex w-screen h-10 justify-between items-center">
+      <div><img className=" w-[25vw] h-auto relative p-2" src={logoWhite.src} alt="Website logo" /></div>
+      <div><input className="bg-transparent" type="text" placeholder="Search..."/></div>
       {<button style={{outline: "1px solid black"}} onClick={() => setShow(!show)}>Hamburger</button>}
       {show && 
         <div className="absolute top-0 right-0 m-12 flex flex-col">
         <p>Hallo</p>
         <p>Hallo2</p>
         </div>
-      
-      
       
       }
     </header>
