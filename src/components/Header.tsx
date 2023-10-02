4740ef6b009ce696b0c5181ad610859dbf837611
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-02 18:18:20 +0200

    feature #23: create initial header style and elements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 const Header = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <header>
-      This is the header
-      <button onClick={() => setShow(!show)}>Hamburger</button>
-      {show && <div>Hei</div>}
+    <header style={{outline: "1px dotted red", width: "100vw", display: "flex", margin: "1vw, 0vw, 0vw, 0vw"}}>
+      <div className="image"><img src="" alt="" />Bilde går her</div>
+      <div className="search"><input type="text" placeholder="Search"/></div>
+      <button style={{outline: "1px solid black"}} onClick={() => setShow(!show)}>Hamburger</button>
+      {show && 
+        <React.Fragment>
+          burgermeny-ting
+        </React.Fragment>
+      
+      
+      
+      }
     </header>
   );
 };
