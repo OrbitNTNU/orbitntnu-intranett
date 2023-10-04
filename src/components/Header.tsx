8ee857578a618ef8d7994cd9380bb0c79da792ea
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-04 18:44:07 +0200

    feat #26: added more text to the burger menu that will be links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import logoWhite from "../../public/images/orbitwhite.png"; // Import the image file
 
 
+/* 
+Her er kode for navbaren til siden, resolutionen brukt under designingen er 1512x982 px
+*/
+
 const Header = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <header className="outline-dotted outline-red-500 flex w-screen h-10 justify-between items-center">
-      <div className="flex justify-start"><img className=" h-[6vh] h-auto relative" src={logoWhite.src} alt="Website logo" /></div>
+    <header className="outline-dotted outline-red-500 flex w-screen h-10 justify-between items-center p-2">
+      <div className="flex justify-start"><img className=" h-[6vh] relative" src={logoWhite.src} alt="Website logo" /></div>
       <div><input className="bg-transparent w-[300px] relative right-18" type="text" placeholder="Search..."/></div>
-      {<button style={{outline: "1px solid black"}} onClick={() => setShow(!show)}>Hamburger</button>}
+      {<button style={{outline: "1px solid black"}} className={`${show ? "flex flex-col -gap-3 items-center transition-all duration-300 " : "flex transition-all duration-300 "} `} onClick={() => setShow(!show)}> <p className={`${show ? "-mt-2" : ""}`}>Ham</p>
+  <p className={`${show ? "-mt-2" : ""}`}>bur</p>
+  <p className={`${show ? "-mt-2" : ""}`}>ger</p></button>}
       {show && 
-        <div className="absolute top-0 right-0 m-12 flex flex-col bg-accentColorTwo ">
-        <p>Hallo</p>
-        <p>Hallo2</p>
+        <div className="absolute top-0 right-0 mt-12 flex flex-col bg-accentColorTwo flex justify-start items-end w-40 p-2 rounded-sm">
+        <a href="https://orbitntnu.com/">Your profile</a>
+        <a>Contact HR</a>
+        <a>Calendar</a>
+        <a>Search for orbiter</a>
+        <a>Announcements</a>
+        <a>Blog</a>
+        <a>Legacy page</a>
+        <a>Meme gallery</a>
+
         </div>
       
       }
