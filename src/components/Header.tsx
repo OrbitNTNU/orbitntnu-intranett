f12f2ea0377479fadd128aee45de8555b081468b
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-04 20:01:00 +0200

    feat #26: increase navbar height

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,25 @@ const Header = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <header className="outline-dotted outline-red-500 flex w-screen h-10 justify-between items-center p-2">
-      <div className="flex justify-start"><img className=" h-[6vh] relative" src={logoWhite.src} alt="Website logo" /></div>
-      <div><input className="bg-transparent w-[300px] relative right-18" type="text" placeholder="Search..."/></div>
-      {<button style={{outline: "1px solid black"}} className={`${show ? "flex flex-col -gap-3 items-center transition-all duration-300 " : "flex transition-all duration-300 "} `} onClick={() => setShow(!show)}> <p className={`${show ? "-mt-2" : ""}`}>Ham</p>
-  <p className={`${show ? "-mt-2" : ""}`}>bur</p>
-  <p className={`${show ? "-mt-2" : ""}`}>ger</p></button>}
+    <header className="outline-dotted outline-red-500 flex w-screen h-16 justify-between items-center p-1">
+      
+      <div className="flex justify-start">
+        <a href="/">
+          <img className=" h-[8vh] relative" src={logoWhite.src} alt="Website logo"/>
+        </a>
+      </div>
+
+      <div>
+        <input className="bg-transparent w-[300px] relative right-18" type="text" placeholder="Search..."/>
+      </div>
+      
+      
+      {<button  className={`${show ? "flex flex-col -gap-3 items-center transition-all duration-300 w-24" : "flex w-24 "} `} onClick={() => setShow(!show)}>  
+        <p className={`${show ? "-mt-2" : ""}`}>Ham</p>
+        <p className={`${show ? "-mt-2" : ""}`}>bur</p>
+        <p className={`${show ? "-mt-2" : ""}`}>ger</p></button>}
       {show && 
-        <div className="absolute top-0 right-0 mt-12 flex flex-col bg-accentColorTwo flex justify-start items-end w-40 p-2 rounded-sm">
+        <div className="absolute top-0 right-0 mt-16 flex flex-col bg-accentColorTwo flex justify-start items-end w-40 p-2 rounded-sm">
         <a href="https://orbitntnu.com/">Your profile</a>
         <a>Contact HR</a>
         <a>Calendar</a>
