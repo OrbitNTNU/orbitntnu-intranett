f9554b41e056845be9b5d73447b44808b433e1aa
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-19 18:05:55 +0200

    feat #53: initial create blogpost component

---
+++ src/components/blog/BlogPost.tsx
@@ -0,0 +1,9 @@
+import { BlogPostInterface } from "@/interfaces/BlogPostInterface";
+import React from "react";
+
+const BlogPost = () => {
+    return (
+        <div>Hello</div>
+    )
+} 
+export default BlogPost
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,14 @@
 import Layout from "@/templates/Layout"
-
+import BlogPost from "@/components/blog/blogPost";
 const blog = () => {
 
     return (
         <Layout>
-            <div className="flex justify-center">
+            <div className="flex flex-col justify-center">
                 <p>
                     This is the blog
                 </p>
+                <BlogPost/>
             </div>
         </Layout>
     )
