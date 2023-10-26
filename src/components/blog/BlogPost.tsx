5593c4978e7ce4c19c94d5ff8938b3ee940ae327
B1uskyy <mats.kvanvik@hotmail.com>
2023-10-26 18:58:55 +0200

    chore #53: minor style change for blogpost

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -3,16 +3,7 @@ import React from "react";
 
 const BlogPost = (post: BlogPostInterface) => {
   const { title, announcement, announcementId, postTime, memberID } = post;
-  const maxCharacters = 200;
-
-  const blogPostData: BlogPostInterface = {
-    postTime: new Date(),
-    title: "This is the blog title!",
-    announcement:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    announcementId: 0o0123,
-    memberID,
-  };
+  const maxCharacters = 150;
 
   /*
     The content part of the BlogPost should not be the enite length of the blogpost (if it is a long post)
@@ -20,19 +11,18 @@ const BlogPost = (post: BlogPostInterface) => {
   */
 
   const truncatedContent =
-    blogPostData.announcement &&
-    blogPostData.announcement.length > maxCharacters
-      ? `${blogPostData.announcement.substring(0, maxCharacters)}...`
-      : blogPostData.announcement || ""; // Provide a default value if announcement is falsy
+    announcement && announcement.length > maxCharacters
+      ? `${announcement.substring(0, maxCharacters)}...`
+      : announcement || ""; // Provide a default value if announcement is falsy
 
   return (
     <div className="max-w-74vw m-3 flex flex-col items-center justify-between outline">
-      <h1>{blogPostData.title}</h1>
+      <h1>{title}</h1>
       <p className="m-3 text-center">{truncatedContent}</p>
       <button className="mb-2 h-7 w-24 rounded-md bg-[#EAB308] font-medium">
         Read More
       </button>
-      <p className="text-sm">Posted: {blogPostData.postTime.toDateString()}</p>
+      <p className="text-sm">Posted: {postTime.toDateString()}</p>
     </div>
   );
 };
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,10 +1,14 @@
 import Layout from "@/templates/Layout";
 import BlogPost from "@/components/blog/BlogPost";
+import Posts from "@/views/PostsView";
+import post from "@/mockdata/MockBlogPost";
+import { BlogPosts } from "@/views/BlogView";
+
 const blog = () => {
   return (
     <Layout>
       <div className="flex flex-col justify-start">
-        <BlogPost />
+        <BlogPosts posts={post} />
       </div>
     </Layout>
   );
