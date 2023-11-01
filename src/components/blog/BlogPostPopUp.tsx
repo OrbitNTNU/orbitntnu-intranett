c9e232ed105f98def9041fcdbc1cfe04a125eaf4
B1uskyy <mats.kvanvik@hotmail.com>
2023-11-01 19:58:31 +0100

    feat #53: started blogpopup page
    Also commented out the blogposts I made so I can see the design on the blog page

---
+++ src/components/blog/BlogPostPopUp.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { BlogPostInterface } from "@/interfaces/BlogPostInterface";
+
+const BlogPostPopUp = () => {
+  return <h1>Hello</h1>;
+};
+
+export default BlogPostPopUp;
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,14 +1,16 @@
 import Layout from "@/templates/Layout";
-import BlogPost from "@/components/blog/BlogPost";
-import Posts from "@/views/PostsView";
 import post from "@/mockdata/MockBlogPost";
 import { BlogPosts } from "@/views/BlogView";
+import BlogPostPopUp from "@/components/blog/BlogPostPopUp";
 
 const blog = () => {
   return (
     <Layout>
-      <div className="flex flex-col justify-start">
+      {/* <div className="flex flex-col justify-start">
         <BlogPosts posts={post} />
+      </div> */}
+      <div className="">
+        <BlogPostPopUp />
       </div>
     </Layout>
   );
