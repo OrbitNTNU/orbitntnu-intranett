c65fc18b57a32cae7b53d7cb2594a9274a22e2f5
Hanne <hanne.haakaas@gmail.com>
2024-02-14 19:03:33 +0100

    Rebase: views folder

---
+++ src/views/LandingPageViews/AnnouncementView/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ShortAnnouncements = () => {
+    return (
+        <section>
+            heiheihei
+        </section>
+    )
+}
+
+export default ShortAnnouncements;
