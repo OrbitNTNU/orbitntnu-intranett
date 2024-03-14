2078a398997453c87fe7320c19bdd1e9e327d806
Hanne <hanne.haakaas@gmail.com>
2024-03-14 14:44:01 +0100

    Update: External links open in new tab

--- src/views/LandingPageViews/ShortcutsView/index.tsx
+++ src/views/LandingPageViews/ShortcutsView/index.tsx
@@ -8,15 +8,15 @@ export const Shortcuts = () => {
             <h2 className="flex font-bold text-5xl mb-4">
                 Shortcuts
             </h2>
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col justify-center gap-4">
 
                 {/* Display each group of shortcuts */}
                 {mockShortcuts.map((shortcutGroup) => (
                     <div key={shortcutGroup.type}>
                         <h2>{shortcutGroup.type}</h2>
-                        <div className="flex flex-row flex-wrap">
+                        <div className="flex flex-row flex-wrap justify-center">
                             {shortcutGroup.shortcuts.map((infoLink) => (
-                                <Shortcut key={infoLink.header} url={infoLink.url} header={infoLink.header} description={infoLink.description}/>
+                                <Shortcut key={infoLink.header} type={shortcutGroup.type} shortcut={infoLink}/>
                             ))}
                         </div>                            
                     </div>
