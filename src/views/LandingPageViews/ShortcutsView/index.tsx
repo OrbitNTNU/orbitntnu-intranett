547201ccb651bd443a9a2247dd47b7506d92ce60
Hanne <hanne.haakaas@gmail.com>
2024-03-14 14:02:19 +0100

    Update: Shortcuts grouped by internal/external

--- src/views/LandingPageViews/ShortcutsView/index.tsx
+++ src/views/LandingPageViews/ShortcutsView/index.tsx
@@ -5,13 +5,23 @@ export const Shortcuts = () => {
 
     return (
         <div>
-            <h2 className="flex justify-center font-bold text-5xl mb-4">
+            <h2 className="flex font-bold text-5xl mb-4">
                 Shortcuts
             </h2>
-            <div className="flex flex-row flex-wrap justify-center">
-                {mockShortcuts.map((infoLink) => (
-                    <Shortcut key={infoLink.header} url={infoLink.url} header={infoLink.header} description={infoLink.description}/>
+            <div className="flex flex-col justify-center">
+
+                {/* Display each group of shortcuts */}
+                {mockShortcuts.map((shortcutGroup) => (
+                    <div key={shortcutGroup.type}>
+                        <h2>{shortcutGroup.type}</h2>
+                        <div className="flex flex-row flex-wrap">
+                            {shortcutGroup.shortcuts.map((infoLink) => (
+                                <Shortcut key={infoLink.header} url={infoLink.url} header={infoLink.header} description={infoLink.description}/>
+                            ))}
+                        </div>                            
+                    </div>
                 ))}
+
             </div>
         </div>
     )
